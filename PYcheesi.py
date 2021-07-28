c13a1886a584eef93646837b9c8ec67a349438a2
E-prog3 <afropo12@gmail.com>
2021-07-28 04:27:05 -0700

    Added PYcheesi Repository

---
+++ PYcheesi.py
@@ -0,0 +1,18 @@
+# [----------------|Pre-Game-Questions|------------------------------------------------->
+total_CPU_players = int(input("How many Challengers will you face?: "))
+HMN_player_Name = input('State your Name!: ')
+print('Hello ' + str(HMN_player_Name).capitalize() + ',\n\t What color Token will you choose?: ')
+"""{HMN_input = input("L0g1n?:>\\ ")}----------Old-version--> 
+insert time thing for delay
+"""
+
+# [----------------|Know-Thy-Enemy-as-Yourself|----------------------------------------->
+HMN_Color_input = input('')
+
+if HMN_Color_input == 'Black' or HMN_Color_input == 'black':
+    print('Welcome to the dark side!')
+
+
+# CPU Then proceeds to randomly pick color of token
+
+# [----------------|Meat-and-Potatoes|---------------------------------------------------->
